293a55c3ace93bcb3650b43fbb6041098f514740
Ahmed Ali <Ahmed.A.Qenawy@gmail.com>
2023-07-10 01:43:10 +0300

    adding routers structure to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
+import AppLayout from "./ui/AppLayout";
+import Cart from "./features/cart/Cart";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/home", element: <Home /> },
+      { path: "/menu", element: <Menu /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order", element: <CreateOrder /> },
+      { path: "/order/:id", element: <Order /> },
+    ],
+  },
+]);
 
 function App() {
-  return <div>Hell World</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
